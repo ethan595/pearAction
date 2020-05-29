b672bcc0cf6dd6e516f8da767b3fe96d3559bd01
nancy <n@pear.hk>
2020-05-29 14:20:05 +0800

    delete the third package

--- coolsnowwolf/diy.sh
+++ coolsnowwolf/diy.sh
@@ -2,23 +2,6 @@
 # 本脚本工作目录必须是git仓库的主目录
 
 # Add Some Package
-
-# mkdir -p package/custom
-# cd package/custom
-# git clone https://github.com/coolsnowwolf/packages
-# git clone https://github.com/kenzok8/small
-# git clone https://github.com/kenzok8/openwrt-packages
-# git clone https://github.com/Lienol/openwrt-package
-# git clone https://github.com/fw876/helloworld
-# git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-# git clone https://github.com/vernesong/OpenClash
-# git clone https://github.com/tty228/luci-app-serverchan
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome
-# git clone https://github.com/pymumu/luci-app-smartdns
-# git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
-# cd ../../
-
-
 cd package
 git clone  https://github.com/PearInc/openwrt_docker.git
 mv openwrt_docker docker
--- x86_64/diy.sh
+++ x86_64/diy.sh
@@ -3,20 +3,10 @@
 
 # Add Some Package
 
-# mkdir -p package/custom
-# cd package/custom
-# git clone https://github.com/coolsnowwolf/packages
-# git clone https://github.com/kenzok8/small
-# git clone https://github.com/kenzok8/openwrt-packages
-# git clone https://github.com/Lienol/openwrt-package
-# git clone https://github.com/fw876/helloworld
-# git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-# git clone https://github.com/vernesong/OpenClash
-# git clone https://github.com/tty228/luci-app-serverchan
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome
-# git clone https://github.com/pymumu/luci-app-smartdns
-# git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
-# cd ../../
+cd package
+git clone  https://github.com/PearInc/openwrt_docker.git
+mv openwrt_docker docker
+cd ../
 
 # Modify default IP
 
